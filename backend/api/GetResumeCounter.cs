488bb0fe3cbca7433a068225e9be7bd625dcda24
Rishiraj01 <rishirajsharma231@gmail.com>
2022-08-01 16:05:33 +0530

    created a working function

--- backend/api/GetResumeCounter.cs
+++ backend/api/GetResumeCounter.cs
@@ -17,17 +17,20 @@ namespace Company.Function
         [FunctionName("GetResumeCounter")]
         public static HttpResponseMessage Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            [CosmosDB(databaseName:"AzureResume", collectionName: "Counter", ConnectionStringSetting = "AzureResumeConnectionString", Id = "1", PartitionKey = "1")] Counter counter,
-            [CosmosDB(databaseName:"AzureResume", collectionName: "Counter", ConnectionStringSetting = "AzureResumeConnectionString", Id = "1", PartitionKey = "1")] out Counter updatedCounter,
+             [CosmosDB(databaseName:"Resume-ri", collectionName: "counter",
+                ConnectionStringSetting = "Resume-riConnectionString", Id = "1", PartitionKey = "1")] Counter counter,
+                [CosmosDB(databaseName:"Resume-ri", collectionName: "counter",
+                ConnectionStringSetting = "Resume-riConnectionString", Id = "index", PartitionKey = "1")] out Counter updatedCounter,
+
             ILogger log)
         {
-            // Here is where the counter gets updated
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             updatedCounter = counter;
             updatedCounter.Count += 1;
 
             var jsonToReturn = JsonConvert.SerializeObject(counter);
+
             return new HttpResponseMessage(System.Net.HttpStatusCode.OK)
             {
                 Content = new StringContent(jsonToReturn, Encoding.UTF8, "application/json")
--- frontend/main.js
+++ frontend/main.js
@@ -1,20 +1,22 @@
-window.addEventListener('DOMContentLoaded', (event) =>{
-    getVisitCount()
-})
+window.addEventListener('DOMContentLoaded', (event) => {
+    getVisitCount();
+});
 
-const functionApiUrl = 'https://drewdavisgetresumecounter.azurewebsites.net/api/GetResumeCounter?code=azRx9Y9P8A7JrilV2/rqOarjQagF3a2bxJWWF9uioza9pCMOuV8sKA==';
-const localfunctionApi = 'http://localhost:7071/api/GetResumeCounter';
+
+const functionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApiUrl).then(response => {
+    fetch(functionApi)
+    .then(response => {
         return response.json()
-    }).then(response =>{
+    })
+    .then(response => {
         console.log("Website called function API.");
         count = response.count;
-        document.getElementById("counter").innerText = count;
-    }).catch(function(error){
+        document.getElementById('counter').innerText = count;
+    }).catch(function(error) {
         console.log(error);
-    });
+      });
     return count;
 }
